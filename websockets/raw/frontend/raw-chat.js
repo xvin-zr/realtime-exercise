df31037638334671cf2dac0533c6ff6651b685fd
xvin <xvin@bupt.edu.cn>
2024-07-29 13:06:07 +0800

    Simply formatting

--- websockets/raw/frontend/raw-chat.js
+++ websockets/raw/frontend/raw-chat.js
@@ -1,17 +1,17 @@
-const chat = document.getElementById("chat");
-const msgs = document.getElementById("msgs");
-const presence = document.getElementById("presence-indicator");
+const chat = document.getElementById('chat');
+const msgs = document.getElementById('msgs');
+const presence = document.getElementById('presence-indicator');
 let allChat = [];
 
 // listen for events on the form
-chat.addEventListener("submit", function (e) {
-  e.preventDefault();
-  postNewMsg(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
+chat.addEventListener('submit', function (e) {
+    e.preventDefault();
+    postNewMsg(chat.elements.user.value, chat.elements.text.value);
+    chat.elements.text.value = '';
 });
 
 async function postNewMsg(user, text) {
-  // code goes here
+    // code goes here
 }
 
 /*
@@ -21,9 +21,9 @@ async function postNewMsg(user, text) {
  */
 
 function render() {
-  const html = allChat.map(({ user, text }) => template(user, text));
-  msgs.innerHTML = html.join("\n");
+    const html = allChat.map(({ user, text }) => template(user, text));
+    msgs.innerHTML = html.join('\n');
 }
 
 const template = (user, msg) =>
-  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+    `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
